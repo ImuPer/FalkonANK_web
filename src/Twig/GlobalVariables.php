c010ec20adceab1fc7927ec1b4dc8783d8cf2389
Imuhotepu <125667486+Imuhotepu@users.noreply.github.com>
2025-06-23 11:20:54 +0200

    optimization variable global

--- src/Twig/GlobalVariables.php
+++ src/Twig/GlobalVariables.php
@@ -4,30 +4,48 @@ namespace App\Twig;
 
 use App\Repository\AdsRepository;
 use App\Repository\ShopRepository;
+use App\Repository\CityRepository;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\GlobalsInterface;
-use App\Repository\CityRepository;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class GlobalVariables extends AbstractExtension implements GlobalsInterface
 {
     private $cityRepository;
     private $adsRepository;
     private $shopRepository;
+    private $cache;
 
-    public function __construct(CityRepository $cityRepository, AdsRepository $adsRepository, ShopRepository $shopRepository)
-    {
+    public function __construct(
+        CityRepository $cityRepository,
+        AdsRepository $adsRepository,
+        ShopRepository $shopRepository,
+        CacheInterface $cache
+    ) {
         $this->cityRepository = $cityRepository;
         $this->adsRepository = $adsRepository;
         $this->shopRepository = $shopRepository;
+        $this->cache = $cache;
     }
 
     public function getGlobals(): array
     {
+        $cities = $this->cache->get('globals.cities', function() {
+            return $this->cityRepository->findBy([], null, 50);
+        });
+
+        $adss = $this->cache->get('globals.adss', function() {
+            return $this->adsRepository->findBy([], null, 50);
+        });
+
+        $shops = $this->cache->get('globals.shops_lojas', function() {
+            return $this->shopRepository->findBy([], null, 50);
+        });
+
         return [
-            'cities' => $this->cityRepository->findBy([], null, 50),
-            'adss' => $this->adsRepository->findBy([], null, 50),
-            'shops_lojas' => $this->shopRepository->findBy([], null, 50),
+            'cities' => $cities,
+            'adss' => $adss,
+            'shops_lojas' => $shops,
         ];
     }
-
 }
