576296b58515f2d57284e4abd8a10884f6e8aec6
Imuhotepu <125667486+Imuhotepu@users.noreply.github.com>
2026-05-17 19:16:27 +0200

    music_views

--- src/Controller/MusicController.php
+++ src/Controller/MusicController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Music;
 use App\Repository\MusicRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -39,6 +41,24 @@ class MusicController extends AbstractController
         return $this->render('music/index.html.twig', [
             'product' => $product,
             'musics' => $musics,
+        ]);
+    }
+
+
+    #[Route('/music/{id}/view', name: 'app_music_view', methods: ['POST'])]
+    public function incrementView(
+        Music $music,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+
+        $music->setViews($music->getViews() + 1);
+
+        $entityManager->persist($music);
+        $entityManager->flush();
+
+        return new JsonResponse([
+            'success' => true,
+            'views' => $music->getViews()
         ]);
     }
 
