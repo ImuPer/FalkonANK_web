9b0e6450a006ed300daf3db492817bf0069f0352
Imuhotepu <125667486+Imuhotepu@users.noreply.github.com>
2026-05-21 08:58:23 +0200

    purchasing

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,17 +11,33 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
-    {
-         // get the login error if there is one
-         $error = $authenticationUtils->getLastAuthenticationError();
+    public function index(
+        Request $request,
+        AuthenticationUtils $authenticationUtils
+    ): Response {
 
-         // last username entered by the user
-         $lastUsername = $authenticationUtils->getLastUsername();
+        // if already logged in
+        if ($this->getUser()) {
+
+            $redirect = $request->query->get('redirect');
+
+            if ($redirect) {
+                return $this->redirect($redirect);
+            }
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        // get login error
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
-            'error'         => $error,
+            'error' => $error,
+            'redirect' => $request->query->get('redirect'),
         ]);
     }
 }
